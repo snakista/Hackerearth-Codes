5a8594b7462ca1343f50dd53369159fadf0f75f8
Sitanshu Yadav <sitanshyadav@gmail.com>
2020-05-12 16:42:33 +0530

    Add files via upload

---
+++ Mike and LMS.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define l long int
+#define pb push_back
+#define fast ios_base::sync_with_stdio(false)
+int main()
+{
+   fast;
+   cin.tie(0);
+   cout.tie(0);
+   string s;
+   cin>>s;
+   string s1;
+   cin>>s1;
+   string mx="";
+   mx=max(s,s1);
+   cout<<mx;
+   
+   
+   
+}
+
